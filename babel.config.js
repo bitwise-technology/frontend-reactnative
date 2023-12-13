ac925eeb35f9d88a91c6d647b6f6d3d654688e42
Virgínia Maia <virginiamaiabf@gmail.com>
2023-12-13 11:04:06 -0300

    chore: update to facilitate imports

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            modules: './src/modules',
+            shared: './src/shared',
+            services: './src/services',
+            repositories: './src/repositories',
+            providers: './src/providers',
+            assets: './assets',
+          },
+        },
+      ],
+    ],
   };
 };
