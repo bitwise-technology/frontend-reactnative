bbde860329f421354389d5b9bd4cd4f714916b7f
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2023-12-19 14:48:29 -0300

    chore: adding reactotron debugger

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import AuthStackLogin from './src/router/AuthSignIn.router';
 import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import reactotron from './src/reactotron';
+if (__DEV__) reactotron.connect();
 
 export default function App() {
   return (
---
+++ src/reactotron.ts
@@ -0,0 +1,19 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NativeModules } from "react-native";
+import reactotron from "reactotron-react-native";
+
+const { scriptURL } = NativeModules.SourceCode;
+
+const hostName = scriptURL.split("://")[1].split(":")[0];
+
+reactotron.setAsyncStorageHandler!!(AsyncStorage)
+  .configure({
+    host: hostName,
+  })
+  .useReactNative({
+    networking: {
+      ignoreUrls: /logs|symbolicate/,
+    },
+  });
+
+export default reactotron;
